a687b4cc4405d83d43f98a9dacfd9b8998a6e483
Pierluigi Lazazzera <pierluigi.lazazzera@gmail.com>
2016-11-24 17:01:46 +0100

    almost done, I've to finish sub-grid for 8 grids the first 1 is ok

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,8 +2,120 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
+	SudokuVerifier() {
+	};
+
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+		int ris = 0;
+
+		boolean validInput = SudokuVerifier.areNumbersOfSolutionCorrect(candidateSolution);
+		if (!validInput) {
+			return -1;
+		}
+
+		boolean controlRow = true;
+		controlRow = SudokuVerifier.isCorrectRow(candidateSolution);
+		boolean controlColumn = true;
+		controlColumn = SudokuVerifier.isCorrectColumn(candidateSolution);
+		boolean controlSubGrid = true;
+		controlSubGrid = SudokuVerifier.isCorrectSubGrid(candidateSolution);
+
+		if (controlRow == true && controlColumn == true && controlSubGrid == true) {
+			ris = 0;
+		}
+
+		if (controlRow == false) {
+			ris = -3;
+		} else if (controlColumn == false) {
+			ris = -4;
+		} else if (controlSubGrid == false) {
+			ris = -2;
+		}
+
+		return ris;
+
+	}
+
+	private static boolean isCorrectRow(String candidateSolution) {
+		int j = 0;
+		while (j < 80) {
+			String oneRow = candidateSolution.substring(j, j += 9);
+			char[] row = oneRow.toCharArray();
+			for (int i = 0; i < 8; i++) {
+				if (row[i] == row[i + 1]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static boolean isCorrectColumn(String candidateSolution) {
+		int j = 0;
+		int counter = 0;
+		char[] solution = candidateSolution.toCharArray();
+		char[] oneColumn = new char[9];
+
+		while (counter < 9) {
+			for (int i = 0; i < 9; i++) {
+				oneColumn[i] = solution[j];
+				j += 9;
+			}
+			for (int i = 0; i < 8; i++) {
+				if (oneColumn[i] == oneColumn[i + 1]) {
+					return false;
+				}
+			}
+			counter++;
+			j = counter;
+		}
+		return true;
+	}
+
+	private static boolean isCorrectSubGrid(String candidateSolution) {
+		int j = 0;
+		int i = 0;
+		int counter = 0;
+		char[] solution = candidateSolution.toCharArray();
+		char[] charsInOneGrid = new char[9];
+
+		while (counter < 9) {
+			for (i = 0; i < 3; i++) {
+				j = i;
+				charsInOneGrid[i] = solution[j];
+				++counter;
+			}
+			j += 6;
+			for (i = counter; i < 6; i++) {
+				charsInOneGrid[i] = solution[j++];
+				counter++;
+			}
+			j += 6;
+			for (i = counter; i < 9; i++) {
+				charsInOneGrid[i] = solution[j++];
+				counter++;
+			}
+		}
+
+		for (int a = 0; a < 8; a++) {
+			if (charsInOneGrid[a] == charsInOneGrid[a + 1]) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+	private static boolean areNumbersOfSolutionCorrect(String candidateSolution) {
+		char[] solution = candidateSolution.toCharArray();
+		for (char c : solution) {
+			int n = Character.getNumericValue(c);
+			if (n == 0 /* || solution.length != 81 */) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -1,14 +1,59 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 
+	SudokuVerifier sudoku;
+	String correct;
+	String incorrect;
+
+	@Before
+	public void setUp() {
+		sudoku = new SudokuVerifier();
+		correct = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		incorrect = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void everyNumberIsBetween1And9() {
+		String wrongSolution = "417369825632158947958724310825437169791586432346912758289643571573291684164875293"
+;		// Assert
+		assertEquals(-1, sudoku.verify(wrongSolution));
+	}
+	
+	@Test
+	public void candidateRowsAreCorrect() {
+		// Arrange
+		String wrongRow = "117369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		// Assert
+		assertEquals(-3, sudoku.verify(wrongRow));
+
+	}
+	
+	@Test
+	public void candidateColumnsAreCorrect() {
+		// Arrange
+		String wrongColumn = "417369825432158947958724316825437169791586432346912758289643571573291684164875293";
+		// Assert
+		assertEquals(-4, sudoku.verify(wrongColumn));
+
+	}
+	
+	@Test
+	public void candidateSubGridsAreCorrect() {
+		// Arrange
+		// Assert
+		assertEquals(-2, sudoku.verify(incorrect));
+
+	}
+	
+	@Test
+	public void isCorrectSolution(){
+		assertEquals(0, sudoku.verify(correct));
 	}
 
 }
